143c1b45a760d35921aabfc4f8ce3f4d891c5c6a
Sooze16 <slkpes2002@yahoo.com>
2019-08-03 14:28:50 -0500

    looking for jaws

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,12 @@
 var mysql = require("mysql")
 
 if (process.env.JAWSDB_URL) {
+    console.log('using Jaws');
+
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
+    console.log('using local');
+
     connection = mysql.createConnection({
         host: "localhost",
         user: "Sooze16",
